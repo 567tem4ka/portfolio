78e7ea9e081bb52540bdeaa92e907fbc7fff0c43
567tem4ka <124319057+567tem4ka@users.noreply.github.com>
2023-02-14 08:40:22 +0900

    Add files via upload

---
+++ bobav2.py
@@ -0,0 +1,97 @@
+import math
+
+x = 1
+y = 1.5
+z = 3
+print('y > x')
+print('y > z')
+
+a2 = input()
+print(str(a2))
+print(int(a2))
+print(bool(a2))
+print(float(a2))
+
+
+
+a3 = input('Появился, значит, в Зоне Чёрный сталкер. К лагерю ночью повадился ходить и там сует руку в палатку и говорит:\n'
+           ' «Водички попить!» А если не дашь хлебнуть из фляжки или наружу полезешь — пришибет! А раз мужик один решил пошутить:\n'
+           ' вылез тихо из палатки, надел кожаную перчатку и полез к соседям в палатку. Полез, значит, и попрошайничает жалостно:\n'
+           ' «Водички, водички попить…» А тут из палатки навстречу высовывается рука и за горло его — цап!\n И сиплый голосок отзывается тихонько: «А тебе моя водичка зачем нужна?!»')
+
+
+
+yapon = 123987
+print(log100000(yapon))
+print(log10000(yapon))
+print(log1000(yapon))
+print(log100(yapon))
+print(log10(yapon))
+print(yapon)
+
+
+a5 = 'cheeeeeee'
+#есть if elif else
+#ещё есть in or and not
+
+
+a6 = ok
+if a = b or a = c not d = e and bobr = volosati:
+    if 1 = 1 and 2 = 2:
+        if 3 = 3:
+            ok ='ok'
+        else:
+            ok = 'pupok'
+else:
+    ok = 'sasok'
+
+
+a7 = 'itsbigbraintime'
+x = 4
+if x > 0:
+    x = x - 1
+elif x == 1:
+    print("Единица")
+else:
+    print(1)
+
+
+a8 = 'kiril loh'
+for i in range(10):
+    print('kiril - loh')
+while True:
+    print('БЕЕСКОООНЕЕЕЧНООООСТЬ НЕЕЕЕ ПРИИДЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЛ')
+
+#a9: for работает в обределённом радиусе, а while пока верно определённое условие
+
+a10a = input('stepeni')
+a10b = input('chislo')
+print('блииииин, капец я глупый')
+
+
+#11 while - это когда пока
+
+
+a12 = 'ya nepon'
+for i in range(10):
+    for i in range(1,5):
+        for i in range(1,10,3):
+
+
+a13 = [1,2,3,4,5,6,7,8,'vot']
+
+a14a = [1,2,3,4,5,6,7,8,'vseeshovot']
+a14b = list('vrode kakto tak')
+a14c = ['nevergonnagiveyouuuuuuuuuuuup']
+
+
+a15 = input()
+if isdigit(a15) == True:
+    print('#')
+if 2 = 2
+    print('yavobsheneponyal')
+print(len(a15))
+if str.istitle(a15) == True:
+    print('#')
+for i in range(1,5)
+    print(a15)
